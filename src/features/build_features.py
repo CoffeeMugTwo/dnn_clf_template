e3114aa97d79924fad3bfa9efa962b6d105f1935
Thomas Kittler <thomas.kittler.ext@siemens-healthineers.com>
2020-08-20 12:38:40 +0200

    add: implemented a preprocessing task for a train and test split,       splitting features and labels and creating a simple meta data info       file.
    fix: for a model to be stored the input shape needs to be defined. fixed
         this with the meta_data file created in the preprocessing task.
    
    add: stated to implement a train task, which loads the training data and
         the untrained model.
    
    add: the models are now stored in hdf5 files, since TF had some problems
         with accepting a WindowsPath object as a destination.

---
+++ src/features/build_features.py
@@ -0,0 +1,67 @@
+# Std python libs
+from pathlib import Path
+
+# Libs from other packages
+import luigi
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+class PrepareHeartDataset(luigi.Task):
+    """Split dataset into train and test sample and features and labels."""
+
+    raw_data_path = Path('data/raw/heart.csv')
+
+    meta_data_path = Path('data/processed/heart_meta_data.csv')
+
+    train_x_path = Path('data/processed/heart_x_train.csv')
+    train_y_path = Path('data/processed/heart_y_train.csv')
+    test_x_path = Path('data/processed/heart_x_test.csv')
+    test_y_path = Path('data/processed/heart_y_test.csv')
+
+    def requires(self):
+        return None
+
+    def output(self):
+        """Task run successfully when 4 csv-files have been produced:
+           train_x, train_y, test_x and test_y."""
+        yield luigi.LocalTarget(self.train_x_path)
+        yield luigi.LocalTarget(self.train_y_path)
+        yield luigi.LocalTarget(self.test_x_path)
+        yield luigi.LocalTarget(self.test_y_path)
+        yield luigi.LocalTarget(self.meta_data_path)
+
+    def run(self):
+        """Load the raw data, perfom splits, store to disk."""
+
+        raw_df = pd.read_csv(self.raw_data_path)
+
+        x = raw_df.iloc[:, :-1]
+        y = raw_df.iloc[:, -1]
+
+        x_train, x_test, y_train, y_test = train_test_split(x, y)
+
+        x_train.to_csv(self.train_x_path, index=False)
+        y_train.to_csv(self.train_y_path, index=False)
+        x_test.to_csv(self.test_x_path, index=False)
+        y_test.to_csv(self.test_y_path, index=False)
+
+        x_shape = 1 if len(x.shape)==1 else x.shape[1]
+        y_shape = 1 if len(y.shape)==1 else y.shape[1]
+
+        meta_data = {'x_shape': [x_shape],
+                     'y_shape': [y_shape],
+                     'n_train': [x_train.shape[0]],
+                     'n_test': [x_test.shape[0]]}
+
+        meta_df = pd.DataFrame(data=meta_data)
+
+        meta_df.to_csv(self.meta_data_path, index=False)
+
+        return
+
+
+if __name__ == '__main__':
+    luigi.build([PrepareHeartDataset()],
+                local_scheduler=True,
+                detailed_summary=True)
--- src/models/heart_dnn.py
+++ src/models/heart_dnn.py
@@ -2,35 +2,44 @@ import luigi
 import pandas as pd
 from pathlib import Path
 from luigi.util import inherits
+import h5py
 
-import tensorflow as tf
+
+
+# import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
+
+from src.features.build_features import PrepareHeartDataset
 
 
 class DNNParameters(luigi.Config):
     # Simple config class to hold some globals and config parameters
 
-    untrained_model_path = luigi.Parameter(Path('models/heart_dnn_clf_model_untrained'))
+    untrained_model_path = luigi.Parameter(Path('models/heart_dnn_clf_model_untrained.hdf5'))
 
-    trained_model_path = luigi.Parameter(Path('models/heart_dnn_clf_model_trained'))
-
+    trained_model_path = luigi.Parameter(Path('models/heart_dnn_clf_model_trained.hdf5'))
 
 
 @inherits(DNNParameters)
 class CreateDNNModel(luigi.Task):
 
     def requires(self):
-        return None
+        return PrepareHeartDataset()
 
     def output(self):
-        return luigi.LocalTarget(self.model_path)
+        return luigi.LocalTarget(self.untrained_model_path)
 
     def run(self):
 
+        # load dataset meta info (for the shape of x and y)
+        meta_data = pd.read_csv(PrepareHeartDataset.meta_data_path)
+        x_shape = meta_data['x_shape'][0]
+        y_shape = meta_data['y_shape'][0]
+
         # define model
         model = keras.Sequential([
-            layers.Dense(14, activation='relu', name="input"),
+            layers.Dense(14, activation='relu', input_shape=(x_shape,)),
             layers.Dense(50, activation='relu'),
             layers.Dense(50, activation='relu'),
             layers.Dense(25, activation='relu'),
@@ -43,7 +52,8 @@ class CreateDNNModel(luigi.Task):
                       metrics=['accuracy'])
 
         # Store model
-        model.save(self.model_path)
+        model_file = h5py.File(self.untrained_model_path)
+        model.save(model_file)
 
 
 @inherits(DNNParameters)
@@ -51,9 +61,27 @@ class TrainDNNModel(luigi.Task):
 
     def requires(self):
         yield CreateDNNModel()
-        yield PrepareDataset()
+        yield PrepareHeartDataset()
 
     def output(self):
         return luigi.LocalTarget(self.trained_model_path)
 
     def run(self):
+        """Loads the preprocessed training data and the untrained model, then
+           performs the training. Stores the trained model then on disk."""
+
+        x_train = pd.read_csv(PrepareHeartDataset.train_x_path)
+        y_train = pd.read_csv(PrepareHeartDataset.train_y_path)
+
+        print(x_train)
+        print(y_train)
+
+        model = keras.models.load_model(self.untrained_model_path)
+
+        print(model)
+
+
+if __name__ == '__main__':
+    luigi.build([TrainDNNModel()],
+                local_scheduler=True,
+                detailed_summary=True)
